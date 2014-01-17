39ae40d897ea5e581bfc5b6c09300e6fe51edd32
Mike Bajur <mike@vbot.tv>
2014-01-17 12:50:59 +0100

    Make nested_form validation working well

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,9 +2,5 @@ class Asset < ActiveRecord::Base
   belongs_to :user
   attr_accessible :asset_type, :value
 
-  scope :first_type,  -> { where( asset_type: 1 ) }
-  scope :second_type, -> { where( asset_type: 2 ) }
-  scope :third_type,  -> { where( asset_type: 3 ) }
-
   validates :value, presence: true, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,32 @@
 class User < ActiveRecord::Base
   has_many :assets
 
+  has_many :assets_first_type, class_name: 'Asset', conditions: { asset_type: 1 }
+  has_many :assets_second_type, class_name: 'Asset', conditions: { asset_type: 2 }
+  has_many :assets_third_type, class_name: 'Asset', conditions: { asset_type: 3 }
 
 
-  attr_accessible :email, :assets_attributes
+  attr_accessible :email, 
+                  :assets_attributes, 
+                  :assets_first_type_attributes,
+                  :assets_second_type_attributes,
+                  :assets_third_type_attributes
 
   accepts_nested_attributes_for :assets,
     :allow_destroy => true
 
+  accepts_nested_attributes_for :assets_first_type,
+    :allow_destroy => true
+
+  accepts_nested_attributes_for :assets_second_type,
+    :allow_destroy => true
+
+  accepts_nested_attributes_for :assets_third_type,
+    :allow_destroy => true
+
   validates :email, presence: true
-  validates_associated :assets
-
-  def first_type
-    assets.select {|asset| asset.asset_type == 1}
-  end
-
-  def second_type
-    assets.select {|asset| asset.asset_type == 2}
-  end
-
-  def third_type
-    assets.select {|asset| asset.asset_type == 3}
-  end
+  validates_associated :assets, 
+                       :assets_first_type,
+                       :assets_second_type,
+                       :assets_third_type
 end
